b84ccc48b7b995e784b70233b12d1c8628aad117
Luca Cattani <luca.cattani@mail.polimi.it>
2023-06-05 15:56:56 +0200

    added HA

---
+++ HA.vhd
@@ -0,0 +1,22 @@
+
+library ieee;
+use ieee.std_logic_1164.ALL;
+
+entity HA is
+
+	port(
+		X	 : in  std_logic;
+		CIN	 : in  std_logic;
+		S	 : out std_logic;
+		COUT : out std_logic
+	);
+
+end HA;
+
+architecture RTL of HA is
+begin
+
+	S 	 <= X xor CIN;
+	COUT <= X and CIN;
+
+end RTL;
