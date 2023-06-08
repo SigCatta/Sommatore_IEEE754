535c4aac365031d6abed810a215a843abd3ad8c1
manuela <manuelamarenghi@mail.polimi.it>
2023-06-08 14:29:24 +0200

    zzz

--- SHIFT_RIGHT.vhd
+++ SHIFT_RIGHT.vhd
@@ -15,13 +15,13 @@ architecture STRUCT of SHIFT_RIGHT is
 begin
     with S select
     Y <= X when "000",
-            '0' & X(7 to 1) when "001",
-            "00" & X(7 to 2) when "010",
-            "000" & X(7 to 3) when "011",
-            "0000" & X(7 to 4) when "100",
-            "00000" & X(7 to 5) when "101",
-            "000000" & X(7 to 6) when "110",
-            "0000000" & X(7) when "111",
+            '0' and X(7 to 1) when "001",
+            "00" and X(7 to 2) when "010",
+            "000" and X(7 to 3) when "011",
+            "0000" and X(7 to 4) when "100",
+            "00000" and X(7 to 5) when "101",
+            "000000" and X(7 to 6) when "110",
+            "0000000" and X(7) when "111",
             "00000000" when others; 
 end STRUCT;
 
