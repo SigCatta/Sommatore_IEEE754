1f879afa08c28e5f08a0069b2ab73ff0fef562d5
manuela <manuelamarenghi@mail.polimi.it>
2023-06-08 16:06:36 +0200

    fine shift

--- SHIFT_RIGHT.vhd
+++ SHIFT_RIGHT.vhd
@@ -5,23 +5,38 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SHIFT_RIGHT is
     port(
-        X: in  std_logic_vector(7 downto 0);
-        S: in  std_logic_vector(2 downto 0);
-        Y: out std_logic_vector(7 downto 0)
+        X: in  std_logic_vector(23 downto 0);
+        S: in  std_logic_vector(7 downto 0);
+        Y: out std_logic_vector(23 downto 0)
     );
 end SHIFT_RIGHT;
 
 architecture STRUCT of SHIFT_RIGHT is
 begin
-    with S select
-    Y <= X when "000",
-            '0' and X(7 downto 1) when "001",
-            "00" and X(7 downto 2) when "010",
-            "000" and X(7 downto 3) when "011",
-            "0000" and X(7 downto 4) when "100",
-            "00000" and X(7 downto 5) when "101",
-            "000000" and X(7 downto 6) when "110",
-            "0000000" and X(7) when "111",
-            "00000000" when others; 
+    with S(4 downto 0) select
+    Y <= X when "00000",
+            '0' & X(23 downto 1) when "00001",
+            "00" & X(23 downto 2) when "00010",
+            "000" & X(23 downto 3) when "00011",
+            "0000" & X(23 downto 4) when "00100",
+            "00000" & X(23 downto 5) when "00101",
+            "000000" & X(23 downto 6) when "00110",
+            "0000000" & X(23 downto 7) when "00111",
+            "00000000" & X(23 downto 7) when "01000",
+            "000000000" & X(23 downto 8) when "01001",
+            "0000000000" & X(23 downto 9) when "01010",
+            "00000000000" & X(23 downto 10) when "01011",
+            "000000000000" & X(23 downto 11) when "01100",
+            "0000000000000" & X(23 downto 12) when "01101",
+            "00000000000000" & X(23 downto 13) when "01110",
+            "000000000000000" & X(23 downto 14) when "01111",
+            "0000000000000000" & X(23 downto 15) when "10000",
+            "00000000000000000" & X(23 downto 16) when "10001",
+            "000000000000000000" & X(23 downto 17) when "10010",
+            "0000000000000000000" & X(23 downto 18) when "10011",
+            "00000000000000000000" & X(23 downto 19) when "10100",
+            "000000000000000000000" & X(23 downto 20) when "10101",
+            "0000000000000000000000" & X(23 downto 21) when "10110",
+            "00000000000000000000000" when others; 
 end STRUCT;
 
--- SHIFT_RIGHT_TB.vhd
+++ SHIFT_RIGHT_TB.vhd
@@ -10,17 +10,17 @@ architecture behavior of SHIFT_RIGHT_TB is
  
     component SHIFT_RIGHT
     port(
-      X: in  std_logic_vector(7 downto 0);
-      S: in  std_logic_vector(2 downto 0);
-      Y: out std_logic_vector (7 downto 0)
+      X: in  std_logic_vector(23 downto 0);
+      S: in  std_logic_vector(7 downto 0);
+      Y: out std_logic_vector (23 downto 0)
        );
     end component;
     
     --Inputs
-    signal X   : std_logic_vector(7 downto 0);
-    signal S   : std_logic_vector(2 downto 0);
+    signal X   : std_logic_vector(23 downto 0);
+    signal S   : std_logic_vector(7 downto 0);
     --Outputs
-    signal Y : std_logic_vector(7 downto 0);
+    signal Y : std_logic_vector(23 downto 0);
 begin
  
    UUT: SHIFT_RIGHT 
@@ -33,22 +33,22 @@ begin
  process
  begin		
           
-       X <= "00000000";
-       S  <= "000";
+       X <= "00000000000000000000000";
+       S  <= "00000";
 
        wait for 100 ns;	
 
-       X <= "11100101"; 
-       S <= "110";
+       X <= "111010101111100100101110010"; 
+       S <= "11000";
 
-       -- output should be 00000011
+       -- output should be 00000000000000000000000
           
        wait for 20 ns;
           
-       X <= "01011101"; 
-       S <= "010";
+       X <= "01011101010101011110101"; 
+       S <= "00010";
 
-       -- output should be 00010111
+       -- output should be 00010111010101010111101
        wait;
        end process;
 
