01211d3691196f80deeefca2d61b0100ab58e71a
Luca Cattani <luca.cattani@mail.polimi.it>
2023-06-27 11:55:59 +0200

    reduced clock to 30 ns

--- DENORMALIZED_SUM.vhd
+++ DENORMALIZED_SUM.vhd
@@ -23,23 +23,35 @@ architecture Behavioral of DENORMALIZED_SUM is
 			COUT : out std_logic
 		);
 	end component;
-	 
-	component C2C is								-- calculates the result's 2's complement if necessary
+	
+	component PA is														-- calculates the result's 2's complement if necessary
 		generic(width: integer := 24);
 		port(
-			N    : in  std_logic_vector (width - 1 downto 0);
-			S    : in  std_logic;
-			Z    : out std_logic_vector (width - 1 downto 0);
+			X    : in  std_logic_vector (width - 1 downto 0);
+			CIN  : in  std_logic;
+			S    : out std_logic_vector (width - 1 downto 0);
 			COUT : out std_logic
 		);
 	end component;
 	
+	
+	component MUX is														-- selects between the result and it's 2's complement
+		generic(width : integer := 24);
+		port(
+			X    : in  std_logic_vector (width - 1 downto 0);
+			Y    : in  std_logic_vector (width - 1 downto 0);
+			S    : in  std_logic;
+			Z    : out std_logic_vector (width - 1 downto 0)
+		);
+	end component;
+	
 	signal S:    std_logic_vector(25 downto 0);
+	signal NEGS: std_logic_vector(23 downto 0);
 	signal COUT: std_logic;
 
 begin
 
-	U1: RCA						-- each number has 1 sign bit, 1 extra bit for overflows, 1 extra 1 (not shown in mantissa), 23 mantissa bits
+	U1: RCA							-- each number has 1 sign bit, 1 extra bit for overflows, 1 extra 1 (not shown in mantissa), 23 mantissa bits
 		port map(
 			X    => X(24) & X,	-- adding the extra overflow bit (1 if the number is negative, 0 if positive)
 			Y    => Y(24) & Y,	-- adding the extra overflow bit (1 if the number is negative, 0 if positive)
@@ -50,10 +62,18 @@ begin
 		
 	INCR <= S(24) xor S(25);
 	SIGN <= S(25);
-	
-	U2: C2C
+		
+	U2: PA
 		port map(
-			N => S(23 downto 0),
+			X   => not S(23 downto 0),
+			CIN => '1',
+			S   => NEGS
+		);
+		
+	U3: MUX
+		port map(
+			X => S(23 downto 0),
+			Y => NEGS,
 			S => S(25),
 			Z => M
 		);
--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "secureip" -o "/media/sf_Sommatore_IEEE754/SOMMATORE_IEEE754_TB_isim_par.exe" -prj "/media/sf_Sommatore_IEEE754/SOMMATORE_IEEE754_TB_par.prj" "work.SOMMATORE_IEEE754_TB" 
+-intstyle "ise" -incremental -lib "secureip" -o "/media/sf_Sommatore_IEEE754/SOMMATORE_IEEE754_TB_isim_par.exe" -prj "/media/sf_Sommatore_IEEE754/SOMMATORE_IEEE754_TB_par.prj" "SOMMATORE_IEEE754_TB" 
