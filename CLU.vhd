a8bdb20505ad47c38adfe3c94457d3fadce9d6c0
Luca Cattani <luca.cattani@mail.polimi.it>
2023-06-27 14:46:11 +0200

    simplified CLU logic + refactoring

--- CLU.vhd
+++ CLU.vhd
@@ -8,43 +8,43 @@ entity CLU is
 		SUB  : in  std_logic;
 		PINF : out std_logic;		-- 1 if the result is positive infinity
 		NINF : out std_logic;		-- 1 if the result is negative infinity
-		NAN  : out std_logic
+		NAN  : out std_logic			-- 1 if the result is NaN
 	);
 end CLU;
 
 architecture Behavioral of CLU is
 
 	signal XEXP255 : std_logic;
-	signal XMANN0  : std_logic;		-- 1 if the x's mantissa is not 0
+	signal XMAN0   : std_logic;		-- 1 if the x's mantissa is 0
 	signal XNAN    : std_logic;
 	signal XINF    : std_logic_vector(1 downto 0);		-- 01 if +inf, 11 if -inf ~ if the second bit is 0, X is not inf
 	
 	signal YEXP255 : std_logic;
-	signal YMANN0  : std_logic;		-- 1 if the y's mantissa is not 0
+	signal YMAN0   : std_logic;		-- 1 if the y's mantissa is 0
 	signal YNAN    : std_logic;
 	signal YINF    : std_logic_vector(1 downto 0);		-- 01 if +inf, 11 if -inf ~ if the second bit is 0, Y is not inf
 	
 begin
 
-	XEXP255 <= ((X(30) and X(29)) and (X(28) and X(27))) and ((X(26) and X(25)) and (X(24) and X(23)));
-	XMANN0  <= (((X(22) or X(21)) or (X(20) or X(19))) or ((X(18) or X(17)) or (X(16) or X(15)))) or (((X(14) or X(13)) or (X(12) or X(11))) or ((X(10) or X(9)) or (X(8) or X(7)))) or (((X(6) or X(5)) or (X(4) or X(3))) or ((X(2) or X(1)) or (X(0))));
-	YEXP255 <= ((Y(30) and Y(29)) and (Y(28) and Y(27))) and ((Y(26) and Y(25)) and (Y(24) and Y(23)));
-	YMANN0  <= (((Y(22) or Y(21)) or (Y(20) or Y(19))) or ((Y(18) or Y(17)) or (Y(16) or Y(15)))) or (((Y(14) or Y(13)) or (Y(12) or Y(11))) or ((Y(10) or Y(9)) or (Y(8) or Y(7)))) or (((Y(6) or Y(5)) or (Y(4) or Y(3))) or ((Y(2) or Y(1)) or (Y(0))));
+	XEXP255 <= '1' when X(30 downto 23) = "11111111" else '0';
+	XMAN0   <= '1' when X(22 downto 0) = "00000000000000000000000" else '0';
+	YEXP255 <= '1' when Y(30 downto 23) = "11111111" else '0';
+	YMAN0   <= '1' when Y(22 downto 0) = "00000000000000000000000" else '0';
 	
 	-- set INF
-	XINF <= X(31) & (XEXP255 and (not XMANN0));
-	YINF <= (Y(31) xor SUB) & (YEXP255 and (not YMANN0));
+	XINF <= X(31) & (XEXP255 and XMAN0);
+	YINF <= (Y(31) xor SUB) & (YEXP255 and YMAN0);
 	
-	-- result is +inf if the operands are: inf + inf, inf + n or n + inf 		~ result optimized with karnaugh map
-	PINF <= (( (not XINF(1)) and XINF(0) ) and ( (not YINF(1)) or (not YINF(0)) )) or (( (not YINF(1)) and YINF(0) ) and ( (not XINF(1)) or (not XINF(0)) ));
+	-- result is +inf if the operands are: inf + inf, inf + n or n + inf
+	PINF <= '1' when ((XINF = "01") and (YINF = "01")) or ((XINF = "01") and (YINF /= "11")) or ((XINF /= "11") and (YINF = "01")) else '0';
 	
-	-- result is -inf if the operands are: -inf -inf, -inf +n or n -inf			~ result optimized with karnaugh map
-	NINF <= (( XINF(1) and XINF(0) ) and ( YINF(1) or (not YINF(0)) )) or ((  YINF(1) and YINF(0) ) and ( XINF(1) or (not XINF(0)) ));
+	-- result is -inf if the operands are: -inf -inf, -inf +n or n -inf
+	NINF <= '1' when ((XINF = "11") and (YINF = "11")) or ((XINF = "11") and (YINF /= "01")) or ((XINF /= "01") and (YINF = "11")) else '0';
 		
 	-- set NAN
-	XNAN <= XEXP255 and XMANN0;
-	YNAN <= YEXP255 and YMANN0;
-	NAN  <= (XNAN or YNAN) or ((XINF(1) xor YINF(1)) and (XINF(0) and YINF(0)) );	-- NAN is 1 either x or y are NAN or if x and y are opposite infinities
+	XNAN <= XEXP255 and (not XMAN0);
+	YNAN <= YEXP255 and (not YMAN0);
+	NAN  <= (XNAN or YNAN) or ((XINF(1) xor YINF(1)) and (XINF(0) and YINF(0)) );	-- NAN is if 1 either x or y are NAN or if x and y are opposite infinities
 	
 end Behavioral;
 
